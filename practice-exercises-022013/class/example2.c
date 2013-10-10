0171341f1c60888ae29d091d4fbf4d7e68ca84b7
idontdomath <ramosmejiamartin@gmail.com>
2013-10-10 10:44:41 -0300

    exercises to be discussed in class

---
+++ practice-exercises-022013/class/example2.c
@@ -0,0 +1,5 @@
+/*
+Se tiene un archivo relativo con informacion sobre autos usados en venta (patente, marca, modelo, km, color). 
+Se sabe que el archivo esta ordenado por “color”. 
+Se pide crear un archivo directo que contenga la informacion de todos los autos de color “azul”. 
+*/
